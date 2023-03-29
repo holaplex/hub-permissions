0b0ddbe03c9da7e6ec666521ca3c4893e2adb8d2
mpw <x@mpw.sh>
2023-03-29 13:53:06 -0300

    update workflow + manual build script

--- build-upload-manual.sh
+++ build-upload-manual.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 s3_bucket=stage-holaplex-hub-policies-mpw
 namespace=default
+json_schema_path=policies/graphql/data.json
 schema_raw=$(python3 clean-schema.py schema.graphql) && \
-echo "$schema_raw" && \
-jq --arg data "$schema_raw" '.schema |= $data' policies/graphql/data.json > out.json && \
-cp out.json policies/graphql/data.json && rm out.json && \
+jq --null-input --arg data "$schema_raw" '.schema |= $data' > "$json_schema_path" && \
 opa build -b policies && \
+rm "$json_schema_path" && \
 aws s3 cp bundle.tar.gz s3://"$s3_bucket"/bundle.tar.gz && \
 kubectl rollout restart deploy/hub-permissions-opa -n "$namespace"
